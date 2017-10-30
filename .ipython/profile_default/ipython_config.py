4490c087184df24183737108966ab99b022a0ec6
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-10-30 16:46:05 +0900

    Create ipython config to import pandas and matplotlib on startup

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+c.InteractiveShellApp.exec_lines = [
+    'import numpy as np',
+    'import pandas as pd',
+    'import matplotlib.pyplot as plt',
+    'import seaborn as sns',
+]
+
+c.InteractiveShellApp.matplotlib = 'auto'
