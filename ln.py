4ba4a1d392ebf6b6511f66844ad693e8e24ef258
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-07-28 23:16:55 +0900

    :art: Refactor ln.py

--- ln.py
+++ ln.py
@@ -14,12 +14,12 @@ for x in os.listdir(here):
     if x in ignore or not x.startswith('.'):
         print(x + ": Ignored")
         continue
-    if os.path.exists(os.path.join(home, x)):
+    destination = os.path.join(home, x)
+    if os.path.exists(destination):
         print(x + ": File exists")
-        continue
-    if os.path.lexists(os.path.join(home, x)):
+    elif os.path.lexists(destination):
         print(x + ": Broken link")
-        continue
-    cmd = ["ln", "-s", os.path.join(here, x), os.path.join(home, x)]
-    print(' '.join(cmd))
-    subprocess.call(cmd)
+    else:
+        cmd = ["ln", "-s", os.path.join(here, x), destination]
+        print(' '.join(cmd))
+        subprocess.call(cmd)
