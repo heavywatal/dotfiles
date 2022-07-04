4cccd12522ed933fc3aa3cf298dcba9bb0d37c43
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-07-04 15:09:10 +0900

    :wrench: Enable nbconvert TagRemovePreprocessor

--- .jupyter/jupyter_nbconvert_config.py
+++ .jupyter/jupyter_nbconvert_config.py
@@ -1,1 +1,5 @@
 c.NbConvertBase.display_data_priority = ["text/plain"]
+c.TagRemovePreprocessor.enabled = True
+c.TagRemovePreprocessor.remove_all_outputs_tags = ("remove_output",)
+c.TagRemovePreprocessor.remove_cell_tags = ("remove_cell",)
+c.TagRemovePreprocessor.remove_input_tags = ("remove_input",)
