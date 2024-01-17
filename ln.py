bff901144012e928512a241d0626c9cea035e4df
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-01-17 19:19:44 +0900

    :bug: Fix regression on lustre

--- ln.py
+++ ln.py
@@ -4,32 +4,32 @@ from pathlib import Path
 ignore = [".DS_Store", ".git", ".gitignore", ".vscode"]
 
 
-def _ln_s(target: Path, link: Path) -> None:
+def _ln_s(source: Path, link: Path) -> None:
     if link.exists():
         if link.is_symlink():
-            print("OK:", link.relative_to(Path.home().resolve()))
+            print("OK:", link.relative_to(Path.home()))
         else:
             print("WARNING: Not symlink:", link)
     elif link.is_symlink():
         print("WARNING: Broken link:", link)
     else:
-        print("ln -s", target, link)
-        link.symlink_to(target)
+        print("ln -s", source, link)
+        link.symlink_to(source)
 
 
-for x in sorted(Path(__file__).parent.iterdir()):
+for x in sorted(Path(__file__).resolve().parent.iterdir()):
     if x.name in ignore or not x.name.startswith("."):
         print("# :", x.name)
     else:
-        target = x.relative_to(Path.home().resolve())
+        source = x.relative_to(Path.home().resolve())
         link = Path.home() / x.name
-        _ln_s(target, link)
+        _ln_s(source, link)
 
 
 app_support = Path("~/Library/Application Support").expanduser()
 if app_support.exists():
     xdg_config_home = Path("../../.config")
     for x in ["Code", "ruff"]:
-        target = xdg_config_home / x
+        source = xdg_config_home / x
         link = app_support / x
-        _ln_s(target, link)
+        _ln_s(source, link)
