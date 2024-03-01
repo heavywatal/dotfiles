a0b99af18bf56e115938bc178368f93c22e847e1
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-03-01 18:39:12 +0900

    :lipstick: Customize wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,27 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+config.check_for_updates = false
+config.window_close_confirmation = 'NeverPrompt'
+
+config.font_size = 12.0
+config.font = wezterm.font_with_fallback {
+  'UbuntuMono Nerd Font',
+  'Ubuntu Mono',
+  'Hiragino Sans',
+}
+config.initial_cols = 120
+config.initial_rows = 66
+config.show_tab_index_in_tab_bar = false
+config.tab_bar_at_bottom = false
+config.tab_max_width = 32
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+config.default_cursor_style = 'BlinkingBlock'
+config.cursor_blink_rate = 500
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+
 function get_appearance()
   if wezterm.gui then
     return wezterm.gui.get_appearance()
@@ -8,25 +29,61 @@ function get_appearance()
   return 'Dark'
 end
 
-function scheme_for_appearance()
-  if get_appearance():find 'Dark' then
-    return 'Catppuccin Mocha'
-  else
-    return 'Catppuccin Latte'
-  end
+function modify_alpha(color, alpha)
+  r, g, b, a = wezterm.color.parse(color):srgba_u8()
+  return string.format("rgba(%d %d %d %f)", r, g, b, alpha)
 end
 
-config.initial_cols = 120
-config.initial_rows = 66
-config.window_close_confirmation = 'NeverPrompt'
+local ui_bg
+local bg
+if get_appearance():find 'Dark' then
+  config.color_scheme = 'Catppuccin Mocha'
+  ui_bg = '#3a3a3a'
+  bg = '#1f1f1f'
+else
+  config.color_scheme = 'Catppuccin Latte'
+  ui_bg = '#f2f2f2'
+  bg = '#ffffff'
+end
+local palette = wezterm.color.get_builtin_schemes()[config.color_scheme]
+local fg = palette.foreground
+
 config.window_background_opacity = 0.95
-config.tab_bar_at_bottom = true
-config.color_scheme = scheme_for_appearance()
-config.font_size = 12.0
-config.font = wezterm.font_with_fallback {
-  'UbuntuMono Nerd Font',
-  'Ubuntu Mono',
-  'Hiragino Sans',
+config.window_frame = {
+  font = wezterm.font_with_fallback {
+    'SF Pro',
+    'Helvetica Neue',
+    'Ubuntu',
+  },
+  font_size = 13,
+  active_titlebar_bg = modify_alpha(ui_bg, 0.95),
+  inactive_titlebar_bg = modify_alpha(ui_bg, 0.90),
+}
+config.colors = {
+  background = bg,
+  tab_bar = {
+    inactive_tab_edge = 'none',
+    active_tab = {
+      bg_color = bg,
+      fg_color = fg,
+    },
+    inactive_tab = {
+      bg_color = modify_alpha(bg, 0.4),
+      fg_color = fg,
+    },
+    inactive_tab_hover = {
+      bg_color = modify_alpha(bg, 0.8),
+      fg_color = fg,
+    },
+    new_tab = {
+      bg_color = 'none',
+      fg_color = fg,
+    },
+    new_tab_hover = {
+      bg_color = modify_alpha(bg, 0.4),
+      fg_color = fg,
+    },
+  },
 }
 
 return config
