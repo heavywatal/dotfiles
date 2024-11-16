e6b14b6ab2667e2982a7e5377ac41cb08ec9e683
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-11-16 23:36:31 +0900

    :arrow_up: Replace --print-to-pdf-no-header with --no-pdf-header-footer

--- .config/bin/chromepdf.sh
+++ .config/bin/chromepdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-options=(--headless --run-all-compositor-stages-before-draw --print-to-pdf-no-header)
+options=(--headless --run-all-compositor-stages-before-draw --no-pdf-header-footer)
 if [ $# -lt 2 ]; then
   "$chrome" ${options[@]} --print-to-pdf "$1"
 else
