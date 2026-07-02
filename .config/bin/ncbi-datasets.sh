20867623f959ccb8948402c5a4c6943b53430bad
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-07-02 17:15:38 +0900

    refactor: Enhance curl command in ncbi-datasets.sh

--- .config/bin/ncbi-datasets.sh
+++ .config/bin/ncbi-datasets.sh
@@ -4,14 +4,12 @@ HOST=ftp.ncbi.nlm.nih.gov
 [ "$(uname)" = "Darwin" ] && OS=mac || OS=linux-amd64
 URL_PREFIX=https://${HOST}/pub/datasets/command-line/v2/${OS}
 for cmd in datasets dataformat; do
-  TEMP=$(mktemp)
-  if DST=$(command -v "$cmd"); then
-    if curl -z "$DST" -o "$TEMP" "${URL_PREFIX}/${cmd}" && [ -s "$TEMP" ]; then
-      mv "$TEMP" "$DST"
-    fi
-  else
-    DST="${1:-${HOME}/.local/bin}/${cmd}"
-    curl -o "$TEMP" "${URL_PREFIX}/${cmd}" && mv "$TEMP" "$DST"
+  URL="${URL_PREFIX}/${cmd}"
+  if ! DST=$(command -v "$cmd"); then
+    PREFIX="${1:-${HOME}/.local/bin}"
+    DST="${PREFIX}/${cmd}"
+    mkdir -p "$PREFIX"
   fi
-  [ ! -x "$DST" ] && chmod +x "$DST"
+  TEMP=$(mktemp)
+  curl -fsSL -z "$DST" -o "$TEMP" "$URL" && [ -s "$TEMP" ] && chmod +x "$TEMP" && mv "$TEMP" "$DST"
 done
